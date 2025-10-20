f39a8f3291ab7ec4fad76532cf4f607557209d09
Sagor Majomder <sagormajomder@gmail.com>
2025-10-20 18:58:28 +0600

    Add loading fallback element to news details route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -43,6 +43,7 @@ const router = createBrowserRouter([
   },
   {
     path: "news-details/:id",
+    hydrateFallbackElement: <Loading />,
     element: (
       <PrivateRoute>
         <NewsDetailsPage />
