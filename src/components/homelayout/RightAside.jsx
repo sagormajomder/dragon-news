0b46870e01fe64cf693e6c31169884fda7562a39
Sagor Majomder <sagormajomder@gmail.com>
2025-10-20 20:30:29 +0600

    Add conditional rendering for SocialLogin based on user authentication status

--- src/components/homelayout/RightAside.jsx
+++ src/components/homelayout/RightAside.jsx
@@ -1,11 +1,14 @@
+import { useAuth } from "../../contexts/AuthContext";
 import FindUs from "./FindUs";
 import Qzone from "./Qzone";
 import SocialLogin from "./SocialLogin";
 
 export default function RightAside() {
+  const { user } = useAuth();
+
   return (
     <aside className="sticky top-0 col-span-3 space-y-8">
-      <SocialLogin />
+      {!user && <SocialLogin />}
       <FindUs />
       <Qzone />
     </aside>
